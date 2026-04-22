2e9b11d985e92736aa4591539d395b8aa94718a6
carlbienandreilardizabal-spec <carlbienandreilardizabal@gmail.com>
2026-04-22 18:41:19 +0800

    fix port for render

--- server.js
+++ server.js
@@ -101,4 +101,5 @@ app.patch('/orders/:id', (req, res) => {
 });
 
 // ─────────────────────────────────────────────────────────
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
